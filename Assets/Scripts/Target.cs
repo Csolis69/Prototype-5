48b64131a20eb58dff301abe8226902c185055e3
Csolis69 <98620164+Csolis69@users.noreply.github.com>
2022-06-01 12:00:52 -0400

    sklook

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -32,10 +32,16 @@ public class Target : MonoBehaviour
     {
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
-    
+
+    public int pointValue;
+
+    public ParticleSystem explosionParticle;
+
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
